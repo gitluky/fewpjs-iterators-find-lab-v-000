e1a3225f8ca19397c82124a85b258ed4d7692812
Jonathan Luk <jluk87@gmail.com>
2019-10-16 15:32:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+function superBowlWin(record) {
+  record.find((r) => {
+    if (r.result === "W") {
+      return r.year;
+    } else {
+      return undefined;
+    }
+  })
+  
 }
